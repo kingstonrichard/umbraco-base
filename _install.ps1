c481a340c1e0720eadec9048c0120e0d0e41a205
Richard Kingston <kingstonrichard@users.noreply.github.com>
2025-07-10 15:52:30 +0100

    Update _install.ps1
    Add Invoke-Step function to avoid script repetition

--- _install.ps1
+++ _install.ps1
@@ -1,35 +1,45 @@
-Write-Host 'Installing the Umbraco templates' -ForegroundColor Green
-dotnet new install Umbraco.Templates
-
-Write-Host 'Creating solution and project' -ForegroundColor Green
-dotnet new sln --force
-dotnet new umbraco -n Umbraco -o src/Umbraco --force
-
-Write-Host 'Adding project to solution' -ForegroundColor Green
-dotnet sln add src/Umbraco/Umbraco.csproj
-
-Write-Host 'Adding packages to project' -ForegroundColor Green
+$global:errors = $false
+$global:logFile = ".\installation.log"
 $nugetSource = 'https://api.nuget.org/v3/index.json'
-dotnet add src/Umbraco/Umbraco.csproj package Umbraco.Forms -s $nugetSource
-dotnet add src/Umbraco/Umbraco.csproj package uSync -s $nugetSource
 
-Write-Host 'Initialising new GIT repo' -ForegroundColor Green
-Remove-Item .\.git\ -Recurse -Force -erroraction 'silentlycontinue'
-try {
-    git init
-    git branch -M main
+function Invoke-Step {
+    param (
+        [ScriptBlock]$Command,
+        [string]$Message
+    )
+
+    Write-Host "$Message... " -NoNewline
+    try {
+        $result += & $Command
+        $formattedOutput = ($result -join "`n") + "`n"
+        $formattedOutput | Out-File $global:logFile -Encoding UTF8 -Append
+        Write-Host "Done!" -ForegroundColor Green
+    }
+    catch {
+        Write-Host "Failed!" -ForegroundColor Red
+        $global:errors = $true
+    }
 }
-catch {
-    Write-Host 'Unable to initialise git repo - do you have GIT installed?' -ForegroundColor Red
+
+Invoke-Step { dotnet new install Umbraco.Templates } "Installing the Umbraco templates"
+Invoke-Step { dotnet new sln --force } "Creating a new .NET solution" 
+Invoke-Step { dotnet new umbraco -n Umbraco -o src/Umbraco --force } "Creating new .NET project"
+Invoke-Step { dotnet sln add src/Umbraco/Umbraco.csproj } "Adding project to solution"
+Invoke-Step { dotnet add src/Umbraco/Umbraco.csproj package Umbraco.Forms -s $nugetSource } "Adding Umbraco.Forms package to project"
+Invoke-Step { dotnet add src/Umbraco/Umbraco.csproj package uSync -s $nugetSource } "Adding uSync package to project"
+Invoke-Step { Remove-Item .\.git\ -Recurse -Force -ErrorAction SilentlyContinue } "Removing cloned GIT repo"
+Invoke-Step { git init } "Initialising new GIT repo"
+Invoke-Step { git branch -M main } "Switching to 'main' branch"
+Invoke-Step { Remove-Item src/Umbraco/.gitignore -Force -ErrorAction SilentlyContinue } "Removing duplicate Umbraco .gitignore file"
+Invoke-Step { Remove-Item README.md -Force -ErrorAction SilentlyContinue } "Removing cloned README.md file"
+Invoke-Step { dotnet build src/Umbraco/Umbraco.csproj } "Building solution"
+
+if ($errors) {
+    Write-Host "There were errors during installation. Please see 'installation.log' file for details."
+} else {
+    Write-Host 'Next steps:' -ForegroundColor Yellow
+    Write-Host '1. Add your own remote GIT repo.' -ForegroundColor Yellow
+    Write-Host '2. Open the SLN file and use the command palette to generate build assets.' -ForegroundColor Yellow
+    Write-Host '3. Run the project and import using uSync via Settings.' -ForegroundColor Yellow
+    Remove-Item _install.ps1 -Force -ErrorAction SilentlyContinue
 }
-
-Write-Host 'Finishing up' -ForegroundColor Green
-Remove-Item src/Umbraco/.gitignore
-Remove-Item README.md
-dotnet build src/Umbraco/Umbraco.csproj
-
-Write-Host 'Next steps:' -ForegroundColor Yellow
-Write-Host '1. Add your own remote GIT repo.' -ForegroundColor Yellow
-Write-Host '2. Open the SLN file and use the command palette to generate build assets.' -ForegroundColor Yellow
-Write-Host '3. Run the project and import using uSync via Settings.'
-Remove-Item _install.ps1
