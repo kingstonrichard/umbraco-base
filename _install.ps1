b67934909a76732b851a6664609b28e44808e406
Richard Kingston <richardkingston@gmail.com>
2025-07-18 14:05:45 +0100

    Update install script

--- _install.ps1
+++ _install.ps1
@@ -22,21 +22,22 @@ function Invoke-Step {
 }
 
 Invoke-Step { dotnet new install Umbraco.Templates } "Installing the Umbraco templates"
-Invoke-Step { dotnet new sln --force } "Creating a new .NET solution" 
+Invoke-Step { dotnet new sln --force } "Creating a new .NET solution"
 Invoke-Step { dotnet new umbraco -n Umbraco -o src/Umbraco --force } "Creating new .NET project"
 Invoke-Step { dotnet sln add src/Umbraco/Umbraco.csproj } "Adding project to solution"
 Invoke-Step { dotnet add src/Umbraco/Umbraco.csproj package Umbraco.Forms -s $nugetSource } "Adding Umbraco.Forms package to project"
 Invoke-Step { dotnet add src/Umbraco/Umbraco.csproj package uSync -s $nugetSource } "Adding uSync package to project"
+Invoke-Step { Remove-Item src/Umbraco/.gitignore -Force -ErrorAction SilentlyContinue } "Removing duplicate Umbraco .gitignore file"
 Invoke-Step { Remove-Item .\.git\ -Recurse -Force -ErrorAction SilentlyContinue } "Removing cloned GIT repo"
 Invoke-Step { git init } "Initialising new GIT repo"
 Invoke-Step { git branch -M main } "Switching to 'main' branch"
-Invoke-Step { Remove-Item src/Umbraco/.gitignore -Force -ErrorAction SilentlyContinue } "Removing duplicate Umbraco .gitignore file"
 Invoke-Step { Remove-Item README.md -Force -ErrorAction SilentlyContinue } "Removing cloned README.md file"
-Invoke-Step { dotnet build src/Umbraco/Umbraco.csproj } "Building solution"
+Invoke-Step { dotnet build } "Building solution"
 
 if ($errors) {
     Write-Host "There were errors during installation. Please see 'installation.log' file for details."
-} else {
+}
+else {
     Write-Host 'Next steps:' -ForegroundColor Yellow
     Write-Host '1. Add your own remote GIT repo.' -ForegroundColor Yellow
     Write-Host '2. Open the SLN file and use the command palette to generate build assets.' -ForegroundColor Yellow
